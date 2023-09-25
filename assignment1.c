03a22e7c5876c76a0b1f7cd82735fd34570f0db0
Jamie Grasley <16744724+JamieHour@users.noreply.github.com>
2023-09-25 13:42:36 -0400

    Main Add pseudo code per assignment

---
+++ assignment1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    struct Queue *qptr = NULL;
+
+    build(&qptr, 5);
+    // ====== Beginn Test Case 1
+    enqueue(qptr, 1);
+    enqueue(qptr, 2);
+    enqueue(qptr, 3);
+    enqueue(qptr, 4);
+    enqueue(qptr, 5);
+    enqueue(qptr, 6);
+    printf("Dequeue() %d \n", dequeue(qptr));
+    printf("Dequeue() %d \n", dequeue(qptr));
+    printf("Dequeue() %d \n", dequeue(qptr));
+    printf("Dequeue() %d \n", dequeue(qptr));
+    printf("Dequeue() %d \n", dequeue(qptr));
+    printf("Dequeue() %d \n", dequeue(qptr));
+    // ====== End Test Case 1
+    
+    // free anything that was created with malloc!
+    if (qptr->data) free(qptr->data);
+    if(qptr) free(qptr);
+    printf("\n");
+    return 0;
